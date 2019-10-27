8ebceb406c684e3b628c86ae3990f0e4934702af
Yura <hvalovyura@gmail.com>
2019-10-27 19:46:19 +0300

    simple frame

---
+++ src/Lab2VarA/MainClass.java
@@ -0,0 +1,14 @@
+package Lab2VarA;
+
+import javax.swing.*;
+
+public class MainClass {
+
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
